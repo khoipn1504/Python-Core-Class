e7aff8c9d40dd902da7e552f01127921ad710b10
khoipn1504 <neron0men@gmail.com>
2020-05-21 15:15:55 +0700

    update swap

--- Day_3/Z6.py
+++ Day_3/Z6.py
@@ -2,16 +2,14 @@ def SelectionSort(lstNum):
     for i in range(len(lstNum)):
         for x in range(i, len(lstNum)):
             if lstNum[x] < lstNum[i]:
-                temp=lstNum[i]
-                lstNum[i]=lstNum[x]
-                lstNum[x]=temp
+                lstNum[x], lstNum[i] = lstNum[i], lstNum[x] #swap value
 
 
-def QuickSort(lstNum):
-    
+# def QuickSort(lstNum):
+
 
 lstNum = input('Nhập vào danh sách các số cần sắp xêp: ').strip().split(' ')
-lstNum=[int(i) for i in lstNum]
+lstNum = [int(i) for i in lstNum]
 
 SelectionSort(lstNum)
 
