b17fadf6e43238bc349a0bb8d195ac08fd2b7b23
khoipn1504 <neron0men@gmail.com>
2020-05-23 11:06:43 +0700

    update z6 z7

--- Day_3/Z6.py
+++ Day_3/Z6.py
@@ -2,15 +2,29 @@ def SelectionSort(lstNum):
     for i in range(len(lstNum)):
         for x in range(i, len(lstNum)):
             if lstNum[x] < lstNum[i]:
-                lstNum[x], lstNum[i] = lstNum[i], lstNum[x] #swap value
+                lstNum[x], lstNum[i] = lstNum[i], lstNum[x]  # swap value
 
 
-# def QuickSort(lstNum):
+
+def QuickSort(lstInput):
+    lstNum=lstInput.copy()
+    if len(lstNum) < 2:
+        return lstNum
+
+    pivot = lstNum.pop()
+    lstLow = []
+    lstHigh = []
+    for num in lstNum:
+        if num < pivot:
+            lstLow.append(num)
+        else:
+            lstHigh.append(num)
+
+    return QuickSort(lstLow)+[pivot]+QuickSort(lstHigh)
 
 
 lstNum = input('Nhập vào danh sách các số cần sắp xêp: ').strip().split(' ')
 lstNum = [int(i) for i in lstNum]
-
-SelectionSort(lstNum)
-
+a=QuickSort(lstNum)
+print(a)
 print(lstNum)
---
+++ Day_3/Z7.py
@@ -0,0 +1,10 @@
+lstAB=[int(i) for i in input('Nhap vao 2 so A va N: ').strip().split(' ')]
+
+ans=[]
+for i in range(0,lstAB[0]):
+    row=[]
+    for j in range(0,lstAB[1]):        
+        row.append(i*j)
+    ans.append(row)
+
+print(ans)
